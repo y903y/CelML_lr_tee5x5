6467bee9b5a294aaa871b0a4e1a4280d6f90990e
t-maehama <takaya@t-maehama.(none)>
2013-11-25 12:44:10 +0900

    bug fixed

--- mpi/no_flag/LR_ring_30x30_noflag_mpi_isend_irecv_cross.c
+++ mpi/no_flag/LR_ring_30x30_noflag_mpi_isend_irecv_cross.c
@@ -245,9 +245,9 @@ int main ( int argc , char** argv ) {
 	int nodeindex2[130] = {258, 259, 260, 261, 262, 263, 264, 265, 274, 275, 276, 277, 278, 279, 280, 281, 282, 292, 293, 294, 295, 296, 297, 298, 299, 300, 310, 311, 312, 313, 314, 315, 316, 317, 318, 319, 330, 331, 332, 333, 334, 335, 336, 337, 338, 339, 350, 351, 352, 353, 354, 355, 356, 357, 358, 359, 360, 361, 374, 375, 376, 377, 378, 379, 380, 381, 382, 383, 384, 385, 398, 399, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 422, 423, 424, 425, 426, 427, 428, 429, 430, 431, 432, 444, 445, 446, 447, 448, 449, 450, 451, 452, 453, 464, 465, 466, 467, 468, 469, 470, 471, 472, 482, 483, 484, 485, 486, 487, 488, 489, 498, 499, 500, 501, 502, 503, 510, 511, 512, 513};
 	int nodeindex3[130] = {266, 267, 268, 269, 270, 271, 272, 273, 283, 284, 285, 286, 287, 288, 289, 290, 291, 301, 302, 303, 304, 305, 306, 307, 308, 309, 320, 321, 322, 323, 324, 325, 326, 327, 328, 329, 340, 341, 342, 343, 344, 345, 346, 347, 348, 349, 362, 363, 364, 365, 366, 367, 368, 369, 370, 371, 372, 373, 386, 387, 388, 389, 390, 391, 392, 393, 394, 395, 396, 397, 410, 411, 412, 413, 414, 415, 416, 417, 418, 419, 420, 421, 433, 434, 435, 436, 437, 438, 439, 440, 441, 442, 443, 454, 455, 456, 457, 458, 459, 460, 461, 462, 463, 473, 474, 475, 476, 477, 478, 479, 480, 481, 490, 491, 492, 493, 494, 495, 496, 497, 504, 505, 506, 507, 508, 509, 514, 515, 516, 517};
 
-	for(membrane_time = 0.000000; ( membrane_time <= 500.000000 ) ;membrane_time =  ( membrane_time + deltat ) ){
-		/*Revision*/
-		if(myrank == root){
+	if(myrank == root){
+		for(membrane_time = 0.000000; ( membrane_time <= 500.000000 ) ;membrane_time =  ( membrane_time + deltat ) ){
+			/*Revision*/
 
 			sendarray1[0] = membrane_V__n[1];
 			sendarray1[1] = membrane_V__n[11];
@@ -522,7 +522,11 @@ int main ( int argc , char** argv ) {
 			/* REVISION: insert time counter */
 			timeCount =  ( timeCount + 1 ) ;
 
-		} else if(myrank == 1) {
+		}
+	} else if(myrank == 1) {
+		for(membrane_time = 0.000000; ( membrane_time <= 500.000000 ) ;membrane_time =  ( membrane_time + deltat ) ){
+			/*Revision*/
+
 			sendarray1[0] = membrane_V__n[2];
 			sendarray1[1] = membrane_V__n[12];
 			sendarray1[2] = membrane_V__n[26];
@@ -765,7 +769,11 @@ int main ( int argc , char** argv ) {
 			/* REVISION: insert time counter */
 			timeCount =  ( timeCount + 1 ) ;
 
-		} else if(myrank == 2) {
+		}
+	} else if(myrank == 2) {
+		for(membrane_time = 0.000000; ( membrane_time <= 500.000000 ) ;membrane_time =  ( membrane_time + deltat ) ){
+			/*Revision*/
+
 			sendarray1[0] = membrane_V__n[385];
 			sendarray1[1] = membrane_V__n[409];
 			sendarray1[2] = membrane_V__n[432];
@@ -1008,7 +1016,11 @@ int main ( int argc , char** argv ) {
 			/* REVISION: insert time counter */
 			timeCount =  ( timeCount + 1 ) ;
 
-		} else if(myrank == 3) {
+		}
+	} else if(myrank == 3) {
+		for(membrane_time = 0.000000; ( membrane_time <= 500.000000 ) ;membrane_time =  ( membrane_time + deltat ) ){
+			/*Revision*/
+
 			sendarray1[0] = membrane_V__n[386];
 			sendarray1[1] = membrane_V__n[410];
 			sendarray1[2] = membrane_V__n[433];
@@ -1250,7 +1262,6 @@ int main ( int argc , char** argv ) {
 
 			/* REVISION: insert time counter */
 			timeCount =  ( timeCount + 1 ) ;
-
 		}
 	}
 
